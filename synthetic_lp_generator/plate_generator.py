500e7fa7b7a1aa2adf2bc4b7039e0f7e8b441d3f
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-02-13 14:59:58 +0900

    plate generator script

---
+++ synthetic_lp_generator/plate_generator.py
@@ -0,0 +1,84 @@
+from utils import *
+
+class PlateGenerator:
+    
+    def __init__(self, save_path, random, transformations):
+        
+        self.save_path = save_path
+        self.random = random
+        self.transformations = transformations
+        self.plate_types = ["basic_europe", "basic_north", "commercial_europe", "commercial_north", "green_old", "green_basic"]
+
+        # Basic nums and chars0
+        self.num_ims, self.num_lists = load("digits/digits_white/")
+        self.char_ims, self.char_lists = load("letters/letters_white/")
+
+        # Yellow nums and chars
+        self.num_ims_yellow, self.num_lists_yellow = load("digits/digits_yellow/")
+        self.char_ims_yellow, self.char_lists_yellow = load("letters/letters_yellow/")
+        self.regions_yellow, self.regions_lists_yellow = load("regions/regions_yellow/")
+       
+        # Green nums and chars
+        self.num_ims_green, self.num_lists_green = load("digits/digits_green/")
+        self.char_ims_green, self.char_lists_green = load("letters/letters_green/")
+        self.regions_green, self.regions_lists_green = load("regions/regions_green/")
+        
+    def preprocessing(self, plate, random, plate_types):
+        
+        if random:
+            plate_type = plate_types[int(np.random.choice(np.arange(0, len(plate_types)), p=[0.33, 0.32, 0.15, 0.15, 0.03, 0.02]))]
+            init_digit_types = ["three", "two"]
+            init_digit = init_digit_types[int(np.random.choice(np.arange(0, len(init_digit_types)), p=[0.4, 0.6]))]
+            three_digit = True if init_digit == "three" else False
+
+            plate = "경기01마0101" if plate_type in ["commercial_europe", "commercial_north", "green_old"] else "01마0000"
+            if plate_type in ["commercial_europe", "commercial_north", "green_old", "green_basic"]:
+                three_digit = False
+                    
+        else:
+            if plate[0].isalpha(): 
+                three_digit, plate_type = False, "commercial_europe"
+            elif plate[0].isdigit():
+                three_digit, plate_type = True if len(plate) > 7 else False, "basic_europe"
+        
+        if plate_type in ["commercial_north", "commercial_europe", "green_old"]:
+            
+            split = os.path.splitext(os.path.basename(plate))[0]
+            region_name, digits = split[:2], split[2:]
+            
+        else: digits, region_name = None, None
+        
+        return three_digit, plate, plate_type, digits, region_name
+    
+    def assertion(self, region_name, region_names):
+        
+        assert region_name != None, "Please insert a region name"
+        assert region_name in [os.path.basename(region) for region in region_names], f"Please choose one of these regions: {[os.path.basename(region) for region in region_names]}"
+    
+    def generation(self, plate, save, plate_type, num, region_name):
+        
+        for _ in range(num):
+            plate_path, num_list, num_size, num_size_2, init_size, char_list, regions, num_ims, char_size, char_ims, region_size, all_regions, plate_size = "plates/plate_white.jpg", self.num_lists, (56, 83), None, (13, 36), self.char_lists, None, self.num_ims, (60, 83), self.char_ims, None, self.regions_lists_yellow, (520, 110)
+            
+            three_digit, plate, plate_type, digits, region_name = self.preprocessing(plate, self.random, self.plate_types)
+            
+            self.assertion(region_name, self.regions_lists_yellow) if plate_type in ["commercial_north", "commercial_europe", "green_old"] else 0
+            
+            if plate_type == "basic_north": num_size, init_size, char_size, plate_size = (40, 83), (46, 10), (49, 70), (355, 155)
+                
+            elif plate_type == "commercial_north": plate_path, num_list, num_size, num_size_2, init_size, char_list, regions, num_ims, char_size, char_ims, region_size, all_regions, plate_size = "plates/plate_yellow.jpg", self.num_lists_yellow, (44, 60), (64, 90), (8, 76), self.char_lists_yellow, self.regions_yellow, self.num_ims_yellow, (64, 62), self.char_ims_yellow, (88, 60), self.regions_lists_yellow, (336, 170)
+                
+            elif plate_type == "commercial_europe": plate_path, num_list, char_list, regions, num_ims, char_ims, region_size = "plates/plate_yellow.jpg", self.num_lists_yellow,  self.char_lists_yellow, self.regions_yellow, self.num_ims_yellow, self.char_ims_yellow, (88, 60)
+                
+            elif plate_type == "green_old": plate_path, num_list, num_size, num_size_2, init_size, char_list, regions, num_ims, char_size, char_ims, region_size, all_regions, plate_size = "plates/plate_green.jpg", self.num_lists_green, (44, 60), (64, 90), (8, 76), self.char_lists_green, self.regions_green, self.num_ims_green, (64, 62), self.char_ims_green, (88, 60), self.regions_lists_yellow, (336, 170)
+             
+            elif plate_type == "green_basic": plate_path, num_list, num_size, num_size_2, init_size, char_list, regions, num_ims, char_size, char_ims, all_regions, plate_size = "plates/plate_green.jpg", self.num_lists_green, (60, 65), (80, 90), (8, 78), self.char_lists_green, self.regions_green, self.num_ims_green, (60, 65), self.char_ims_green, self.regions_lists_yellow, (336, 170)
+            
+            generate_plate(plate_path=plate_path, random=self.random,
+                           plate=plate, num_size=num_size, num_size_2=num_size_2, 
+                           num_list=num_list, init_size=init_size, 
+                           char_list=char_list, regions=regions, three_digit = three_digit,
+                           num_ims=num_ims, char_size=char_size, region_name=region_name,
+                           char_ims=char_ims, label_prefix=plate_type,
+                           save_path=self.save_path, region_size=region_size, all_regions=all_regions,
+                           save_=save, plate_size=plate_size, transformations=self.transformations)
