5d6843ca154d5a7fdc6c9e2f10722533f80dc6cb
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-02-21 15:54:13 +0900

    Update base_dataset.py

--- real_lp_generator/data/base_dataset.py
+++ real_lp_generator/data/base_dataset.py
@@ -29,13 +29,19 @@ class SquarePad:
     
     def __call__(self, image):
         
+        # Get width and height of the image
         w, h = image.size
+        
+        # Get max values of the width and height
         max_wh = np.max([w, h])
         hp = int((max_wh - w)/2)
         hp_rem = (max_wh - w)%2
         vp = int((max_wh - h)/2)
         vp_rem = (max_wh - h)%2
+        
+        # Pad the image
         padding = (hp, vp, hp+hp_rem, vp+vp_rem)
+        
         return F.pad(image, padding, 255, 'constant')
 
 class BaseDataset(data.Dataset, ABC):
