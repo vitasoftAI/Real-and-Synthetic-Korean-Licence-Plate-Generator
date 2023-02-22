0c3157072a87218b1e891fb045650eb02c397b6d
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-02-22 11:31:04 +0900

    Update base_dataset.py

--- real_lp_generator/data/base_dataset.py
+++ real_lp_generator/data/base_dataset.py
@@ -225,10 +225,7 @@ def get_transform(opt, params=None, grayscale=False, method=transforms.Interpola
     if opt.preprocess == 'none': transform_list.append(transforms.Lambda(lambda img: __make_power_2(img, base=4, method=method)))
 
     # Flip option
-    if not opt.no_flip:
-        if params is None or 'flip' not in params: transform_list.append(transforms.RandomHorizontalFlip())
-        elif 'flip' in params: transform_list.append(transforms.Lambda(lambda img: __flip(img, params['flip'])))
-            
+    if not opt.no_flip: transform_list.append(transforms.RandomHorizontalFlip()) if (params is None or 'flip' not in params) elif 'flip' in params transform_list.append(transforms.Lambda(lambda img: __flip(img, params['flip'])))
 
     # Convert option
     if convert:
