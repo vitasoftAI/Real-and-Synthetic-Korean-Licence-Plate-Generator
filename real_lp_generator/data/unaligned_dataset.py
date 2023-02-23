9c92c7b603fdbbf433c283e831577407d13ce43d
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-02-23 14:00:07 +0900

    Update unaligned_dataset.py

--- real_lp_generator/data/unaligned_dataset.py
+++ real_lp_generator/data/unaligned_dataset.py
@@ -47,11 +47,9 @@ class UnalignedDataset(BaseDataset):
             self.dir_A = os.path.join(opt.dataroot, "valA")
             self.dir_B = os.path.join(opt.dataroot, "valB")
 
-        
-        self.A_paths = sorted(make_dataset(self.dir_A, opt.max_dataset_size))   # load images from '/path/to/data/trainA'
-        self.B_paths = sorted(make_dataset(self.dir_B, opt.max_dataset_size))    # load images from '/path/to/data/trainB'
-        self.A_size = len(self.A_paths)  # get the size of dataset A
-        self.B_size = len(self.B_paths)  # get the size of dataset B
+        # Get image paths from the abovementioned folders (domain A and B)
+        self.A_paths = sorted(make_dataset(self.dir_A, opt.max_dataset_size))   
+        self.B_paths = sorted(make_dataset(self.dir_B, opt.max_dataset_size))   
 
     def __getitem__(self, index):
         """Return a data point and its metadata information.
@@ -65,9 +63,9 @@ class UnalignedDataset(BaseDataset):
             A_paths (str)    -- image paths
             B_paths (str)    -- image paths
         """
-        A_path = self.A_paths[index % self.A_size]  # make sure index is within then range # qoldiq
+        A_path = self.A_paths[index % len(self.A_paths)]  # make sure index is within then range # qoldiq
         if self.test:
-            index_B = random.randint(0, self.B_size - 1)
+            index_B = random.randint(0, len(self.B_paths) - 1)
             B_path = self.B_paths[index_B]
         else:
             plate_type = os.path.splitext(os.path.basename(A_path))[0].split("__")[1]
@@ -98,4 +96,4 @@ class UnalignedDataset(BaseDataset):
         As we have two datasets with potentially different number of images,
         we take a maximum of
         """
-        return max(self.A_size, self.B_size)
+        return max(len(self.A_paths), len(self.B_paths))
