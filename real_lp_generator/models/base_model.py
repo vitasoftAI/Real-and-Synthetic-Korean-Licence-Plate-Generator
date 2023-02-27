c13ea1a6d23fd99fd7448dabe20503bd076dac3a
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-02-27 14:38:56 +0900

    Update base_model.py

--- real_lp_generator/models/base_model.py
+++ real_lp_generator/models/base_model.py
@@ -35,29 +35,25 @@ class BaseModel(ABC):
         
         """
         
-        This function Initialize the BaseModel class.
-        Parameters:
-            opt (Option class)-- stores all the experiment flags; needs to be a subclass of BaseOptions
-        When creating your custom class, you need to implement your own initialization.
-        In this fucntion, you should first call <BaseModel.__init__(self, opt)>
-        Then, you need to define four lists:
-            -- self.loss_names (str list):          specify the training losses that you want to plot and save.
-            -- self.model_names (str list):         specify the images that you want to display and save.
-            -- self.visual_names (str list):        define networks used in our training.
-            -- self.optimizers (optimizer list):    define and initialize optimizers. You can define one optimizer for each network. If two networks are updated at the same time, you can use itertools.chain to group them. See cycle_gan_model.py for an example.
+        This function initializes the BaseModel class.
+        
+        Arguments:
+            opt - stores all the experiment flags; needs to be a subclass of BaseOptions;
+
         """
-        self.opt = opt
-        self.gpu_ids = opt.gpu_ids
-        self.isTrain = opt.isTrain
+        
+        self.opt, self.gpu_ids, self.isTrain = opt, opt.gpu_ids, opt.isTrain
         self.device = torch.device('cuda:{}'.format(self.gpu_ids[0])) if self.gpu_ids else torch.device('cpu')  # get device name: CPU or GPU
         self.save_dir = os.path.join(opt.checkpoints_dir, opt.name)  # save all the checkpoints to save_dir
-        if opt.preprocess != 'scale_width':  # with [scale_width], input images might have different sizes, which hurts the performance of cudnn.benchmark.
-            torch.backends.cudnn.benchmark = True
-        self.loss_names = []
-        self.model_names = []
-        self.visual_names = []
-        self.optimizers = []
-        self.image_paths = []
+        
+        # Input images might have different sizes, which hurts the performance of cudnn.benchmark.
+        if opt.preprocess != 'scale_width':  torch.backends.cudnn.benchmark = True
+            
+        self.loss_names, self.model_names, self.visual_names, self.optimizers, self.image_paths   = [], [], [], [], []
+         = []
+         = []
+         = []
+         = []
         self.metric = 0  # used for learning rate policy 'plateau'
 
     @staticmethod
