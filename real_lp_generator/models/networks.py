48b2b482f767763dd4d867c3230158a3a45fda67
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-03-03 15:00:03 +0900

    Update networks.py

--- real_lp_generator/models/networks.py
+++ real_lp_generator/models/networks.py
@@ -290,23 +290,19 @@ def init_weights(net, init_type = 'normal', init_gain = 0.02, debug = False):
         
         """
         
+        # Get classname
         classname = m.__class__.__name__
+        
         if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
-            if debug:
-                print(classname)
-            if init_type == 'normal':
-                init.normal_(m.weight.data, 0.0, init_gain)
-            elif init_type == 'xavier':
-                init.xavier_normal_(m.weight.data, gain=init_gain)
-            elif init_type == 'kaiming':
-                init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
-            elif init_type == 'orthogonal':
-                init.orthogonal_(m.weight.data, gain=init_gain)
-            else:
-                raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
-            if hasattr(m, 'bias') and m.bias is not None:
-                init.constant_(m.bias.data, 0.0)
-        elif classname.find('BatchNorm2d') != -1:  # BatchNorm Layer's weight is not a matrix; only normal distribution applies.
+            if debug: print(classname)
+            if init_type == 'normal': init.normal_(m.weight.data, 0.0, init_gain)
+            elif init_type == 'xavier': init.xavier_normal_(m.weight.data, gain=init_gain)
+            elif init_type == 'kaiming': init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+            elif init_type == 'orthogonal': init.orthogonal_(m.weight.data, gain=init_gain)
+            if hasattr(m, 'bias') and m.bias is not None: init.constant_(m.bias.data, 0.0)
+        
+        # BatchNorm Layer's weight is not a matrix; only normal distribution applies.
+        elif classname.find('BatchNorm2d') != -1:  
             init.normal_(m.weight.data, 1.0, init_gain)
             init.constant_(m.bias.data, 0.0)
 
