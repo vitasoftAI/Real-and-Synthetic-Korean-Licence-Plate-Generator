4edf7a74700ab5a72ac6e9bd17125ada4eaa9d4e
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-03-03 14:41:30 +0900

    Update networks.py

--- real_lp_generator/models/networks.py
+++ real_lp_generator/models/networks.py
@@ -243,15 +243,17 @@ def get_scheduler(optimizer, opt):
 
     """
     
+    # Linear LR scheduler
     if opt.lr_policy == 'linear':
-        def lambda_rule(epoch):
-            lr_l = 1.0 - max(0, epoch + opt.epoch_count - opt.n_epochs) / float(opt.n_epochs_decay + 1)
-            return lr_l
-        scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda_rule)
-    elif opt.lr_policy == 'step':
-        scheduler = lr_scheduler.StepLR(optimizer, step_size=opt.lr_decay_iters, gamma=0.1)
-    elif opt.lr_policy == 'plateau':
-        scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.2, threshold=0.01, patience=5)
+        
+        def lambda_rule(epoch): return 1.0 - max(0, epoch + opt.epoch_count - opt.n_epochs) / float(opt.n_epochs_decay + 1)
+        scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda = lambda_rule)
+
+    # Step LR scheduler
+    elif opt.lr_policy == 'step': scheduler = lr_scheduler.StepLR(optimizer, step_size = opt.lr_decay_iters, gamma = 0.1)
+    
+    # Plateau LR scheduler
+    elif opt.lr_policy == 'plateau': scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.2, threshold=0.01, patience=5)
     elif opt.lr_policy == 'cosine':
         scheduler = lr_scheduler.CosineAnnealingLR(optimizer, T_max=opt.n_epochs, eta_min=0)
     else:
