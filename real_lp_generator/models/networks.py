f5d4f1e7c563da86c3875f6726ea0e1b3487d824
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-03-09 09:18:43 +0900

    Update networks.py

--- real_lp_generator/models/networks.py
+++ real_lp_generator/models/networks.py
@@ -100,7 +100,7 @@ def get_norm_layer(norm_type = 'instance'):
         
     """
     
-    assert pad_type in ['batch', 'instance', 'none'], "Please choose a proper norm type."
+    assert norm_type in ['batch', 'instance', 'none'], "Please choose a proper norm type."
     
     if norm_type == 'batch': norm_layer = functools.partial(nn.BatchNorm2d, affine = True, track_running_stats = True)
     elif norm_type == 'instance': norm_layer = functools.partial(nn.InstanceNorm2d, affine = False, track_running_stats = False)
