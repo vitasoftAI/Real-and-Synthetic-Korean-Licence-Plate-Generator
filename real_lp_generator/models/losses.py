6eb2216dffdbfeb2d07de89f11d86e7e35d732f0
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-03-24 11:53:25 +0900

    losses for training

--- real_lp_generator/models/losses.py
+++ real_lp_generator/models/losses.py
@@ -136,21 +136,20 @@ class PatchNCELoss(nn.Module):
         # neg logit
         batch_dim_for_bmm = 1 if self.opt.nce_includes_all_negatives_from_minibatch else self.opt.batch_size
 
-        # reshape features to batch size
-        feat_q = feat_q.view(batch_dim_for_bmm, -1, dim)
-        feat_k = feat_k.view(batch_dim_for_bmm, -1, dim)
+        # Reshape features to batch size
+        feat_q, feat_k= feat_q.view(batch_dim_for_bmm, -1, dim), feat_k.view(batch_dim_for_bmm, -1, dim)
+        
+        # Get number of patches
         npatches = feat_q.size(1)
         l_neg_curbatch = torch.bmm(feat_q, feat_k.transpose(2, 1))
 
-        # diagonal entries are similarity between same features, and hence meaningless.
-        # just fill the diagonal with very small number, which is exp(-10) and almost zero
-        diagonal = torch.eye(npatches, device=feat_q.device, dtype=self.mask_dtype)[None, :, :]
+        # Fill diagonal with very small numbers
+        diagonal = torch.eye(npatches, device = feat_q.device, dtype = self.mask_dtype)[None, :, :]
         l_neg_curbatch.masked_fill_(diagonal, -10.0)
         l_neg = l_neg_curbatch.view(-1, npatches)
+        
+        # Concatenate positive and negative logits
+        out = torch.cat((l_pos, l_neg), dim = 1) / self.opt.nce_T
 
-        out = torch.cat((l_pos, l_neg), dim=1) / self.opt.nce_T
-
-        loss = self.cross_entropy_loss(out, torch.zeros(out.size(0), dtype=torch.long,
-                                                        device=feat_q.device))
-
-        return loss
+        # Compute and return loss value
+        return self.cross_entropy_loss(out, torch.zeros(out.size(0), dtype = torch.long, device = feat_q.device))
