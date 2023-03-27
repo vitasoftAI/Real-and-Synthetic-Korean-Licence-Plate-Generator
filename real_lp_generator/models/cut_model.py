97a406183fc79227d3a636c27d2f0a9a517a51c5
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-03-27 15:09:18 +0900

    Update cut_model.py

--- real_lp_generator/models/cut_model.py
+++ real_lp_generator/models/cut_model.py
@@ -81,23 +81,26 @@ class CUTModel(BaseModel):
         # Model names list
         self.model_names = ['G', 'F', 'D'] if self.isTrain else ['G']
 
-        # define networks (both generator and discriminator)
+        # Initialize generator and discriminator models
         self.netG = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf, opt.netG, opt.normG, not opt.no_dropout, opt.init_type, opt.init_gain, opt.no_antialias, opt.no_antialias_up, self.gpu_ids, opt)
         self.netF = networks.define_F(opt.input_nc, opt.netF, opt.normG, not opt.no_dropout, opt.init_type, opt.init_gain, opt.no_antialias, self.gpu_ids, opt)
 
-        if self.isTrain:
-            self.netD = networks.define_D(opt.output_nc, opt.ndf, opt.netD, opt.n_layers_D, opt.normD, opt.init_type, opt.init_gain, opt.no_antialias, self.gpu_ids, opt)
+        # Discriminator during training
+        if self.isTrain: self.netD = networks.define_D(opt.output_nc, opt.ndf, opt.netD, opt.n_layers_D, opt.normD, opt.init_type, opt.init_gain, opt.no_antialias, self.gpu_ids, opt)
 
-            # define loss functions
+            # Loss functions for training
             self.criterionGAN = losses.GANLoss(opt.gan_mode).to(self.device)
             self.criterionNCE = []
 
-            for nce_layer in self.nce_layers:
-                self.criterionNCE.append(PatchNCELoss(opt).to(self.device))
+            for nce_layer in self.nce_layers: self.criterionNCE.append(PatchNCELoss(opt).to(self.device))
 
             self.criterionIdt = torch.nn.L1Loss().to(self.device)
-            self.optimizer_G = torch.optim.Adam(self.netG.parameters(), lr=opt.lr, betas=(opt.beta1, opt.beta2))
-            self.optimizer_D = torch.optim.Adam(self.netD.parameters(), lr=opt.lr, betas=(opt.beta1, opt.beta2))
+            
+            # Generator optimizer
+            self.optimizer_G = torch.optim.Adam(self.netG.parameters(), lr = opt.lr, betas = (opt.beta1, opt.beta2))
+            
+            # Discriminator optimizer
+            self.optimizer_D = torch.optim.Adam(self.netD.parameters(), lr = opt.lr, betas = (opt.beta1, opt.beta2))
             self.optimizers.append(self.optimizer_G)
             self.optimizers.append(self.optimizer_D)
 
