ebb7d2c3bf04eb9431d13129da0a5b8f87c0a991
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-04-12 11:35:13 +0900

    test options

--- real_lp_generator/options/test_options.py
+++ real_lp_generator/options/test_options.py
@@ -13,14 +13,21 @@ class TestOptions(BaseOptions):
     """
 
     def initialize(self, parser):
-        parser = BaseOptions.initialize(self, parser)  # define shared options
-        parser.add_argument('--results_dir', type=str, default='./results/', help='saves results here.')
-        parser.add_argument('--phase', type=str, default='test', help='train, val, test, etc')
+        
+        # Initialize a parser
+        parser = BaseOptions.initialize(self, parser)  
+        
+        # Add arguments to the parser
+        parser.add_argument('--results_dir', type = str, default = './results/', help = 'saves results here.')
+        parser.add_argument('--phase', type = str, default = 'test', help = 'train, val, test, etc')
         # Dropout and Batchnorm has different behavioir during training and test.
-        parser.add_argument('--eval', action='store_true', help='use eval mode during test time.')
-        parser.add_argument('--num_test', type=int, default=10000, help='how many test images to run')
-
+        parser.add_argument('--eval', action = 'store_true', help = 'use eval mode during test time.')
+        parser.add_argument('--num_test', type = int, default = 10000, help = 'how many test images to run')
         # To avoid cropping, the load_size should be the same as crop_size
-        parser.set_defaults(load_size=parser.get_default('crop_size'))
+        parser.set_defaults(load_size = parser.get_default('crop_size'))
+        
+        # Turn on eval mode
         self.isTrain = False
+        
+        # Return the parser
         return parser
