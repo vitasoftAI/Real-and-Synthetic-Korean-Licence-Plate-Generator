c7783a3da86c336a44a9fd311d7353aadbc85a52
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-04-26 17:03:10 +0900

    Update train_options.py

--- real_lp_generator/options/train_options.py
+++ real_lp_generator/options/train_options.py
@@ -3,7 +3,6 @@ from .base_options import BaseOptions
 
 class TrainOptions(BaseOptions):
     
-    
     """
     
     This class returns base train options.
@@ -11,10 +10,13 @@ class TrainOptions(BaseOptions):
     """
 
     def initialize(self, parser):
+        
+        # Initialize parser
         parser = BaseOptions.initialize(self, parser)
+        
         # visdom and HTML visualization parameters
-        parser.add_argument('--display_freq', type=int, default=100, help='frequency of showing training results on screen')
-        parser.add_argument('--display_ncols', type=int, default=4, help='if positive, display all images in a single visdom web panel with certain number of images per row.')
+        parser.add_argument('--display_freq', type = int, default = 100, help = 'frequency of showing training results on screen')
+        parser.add_argument('--display_ncols', type = int, default = 4, help = 'if positive, display all images in a single visdom web panel with certain number of images per row.')
         parser.add_argument('--display_id', type=int, default=None, help='window id of the web display. Default is random window id')
         parser.add_argument('--display_server', type=str, default="http://localhost", help='visdom server of the web display')
         parser.add_argument('--display_env', type=str, default='main', help='visdom display environment name (default is "main")')
