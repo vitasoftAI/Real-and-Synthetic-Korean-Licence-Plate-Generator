6f99b46faf3cb846f5b8827d9272fab8368512e0
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-04-26 17:06:32 +0900

    train options

--- real_lp_generator/options/train_options.py
+++ real_lp_generator/options/train_options.py
@@ -46,8 +46,8 @@ class TrainOptions(BaseOptions):
         parser.add_argument('--lr_policy', type = str, default = 'linear', help = 'learning rate policy. [linear | step | plateau | cosine]')
         parser.add_argument('--lr_decay_iters', type = int, default = 50, help = 'multiply by a gamma every lr_decay_iters iterations')
 
+        # Train option
         self.isTrain = True
+        
+        # Return parser with arguments
         return parser
-
-    
-    
