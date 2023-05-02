c62ee5956a80fbf6f050577c61d340ae2923a103
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-05-02 14:15:43 +0900

    Update stylegan_networks.py

--- real_lp_generator/models/stylegan_networks.py
+++ real_lp_generator/models/stylegan_networks.py
@@ -684,25 +684,17 @@ class Generator(nn.Module):
 
         if truncation < 1:
             style_t = []
-
             for style in styles:
-                style_t.append(
-                    truncation_latent + truncation * (style - truncation_latent)
-                )
-
+                style_t.append(  truncation_latent + truncation * (style - truncation_latent)  )
             styles = style_t
 
-        if len(styles) < 2:
-            inject_index = self.n_latent
+        if len(styles) < 2: inject_index = self.n_latent
 
-            if len(styles[0].shape) < 3:
-                latent = styles[0].unsqueeze(1).repeat(1, inject_index, 1)
-
+            if len(styles[0].shape) < 3: latent = styles[0].unsqueeze(1).repeat(1, inject_index, 1)
             else: latent = styles[0]
 
         else:
-            if inject_index is None:
-                inject_index = random.randint(1, self.n_latent - 1)
+            if inject_index is None: inject_index = random.randint(1, self.n_latent - 1)
 
             latent = styles[0].unsqueeze(1).repeat(1, inject_index, 1)
             latent2 = styles[1].unsqueeze(1).repeat(1, self.n_latent - inject_index, 1)
@@ -715,13 +707,10 @@ class Generator(nn.Module):
         skip = self.to_rgb1(out, latent[:, 1])
 
         i = 1
-        for conv1, conv2, noise1, noise2, to_rgb in zip(
-            self.convs[::2], self.convs[1::2], noise[1::2], noise[2::2], self.to_rgbs
-        ):
+        for conv1, conv2, noise1, noise2, to_rgb in zip(  self.convs[::2], self.convs[1::2], noise[1::2], noise[2::2], self.to_rgbs  ):
             out = conv1(out, latent[:, i], noise=noise1)
             out = conv2(out, latent[:, i + 1], noise=noise2)
             skip = to_rgb(out, latent[:, i + 2], skip)
-
             i += 2
 
         image = skip
@@ -731,6 +720,23 @@ class Generator(nn.Module):
         else: return image, None
 
 class ConvLayer(nn.Sequential):
+    
+    """
+    
+    This class initializes convolution layer for style GAN. 
+    
+    Parameters:
+        
+        in_channel        - number of channels for the input volume to the convolution operation, int;
+        out_channel       - number of channels for the out volume from the convolution operation, int;
+        kernel_size       - size of the filter of the convolution operation, int;
+        downsample        - whether or not to use downsampling, bool;
+        blur_kernel       - kernel size for blurring, list -> int;
+        bias              - whether or not to use bias, bool;
+        activate          - whether or not to use activation function, bool.
+    
+    """
+    
     def __init__(
         self,
         in_channel,
